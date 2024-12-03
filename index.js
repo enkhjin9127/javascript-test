bd6da152eb9b75785d2cd5742d5bc7e1c548d9e8
enkhjin9127 <ka0enkhjin@gmail.com>
2024-12-03 11:48:32 +0800

    finished spoj tasks

--- index.js
+++ index.js
@@ -78,25 +78,235 @@ p15year = 2;
 p15month = p15month + p15year * 12;
 console.log(p15month);
 // problem 16
+console.log("p16");
 let p16number1 = 16;
-p16number2 = 14;
-p16Largest = Math.max(p16number1, p16number2);
-console.log("The largest number is: " + p16Largest);
+p16number2 = 69;
+if (p16number1 > p16number2) {
+  console.log("The largest number:", p16number1);
+} else {
+  console.log("The largest number:", p16number2);
+}
 // problem 17
-let p17number1 = 9;
-p17number2 = 6;
-p17Smallest = Math.min(p17number1, p17number2);
-console.log("The smallest number is: " + p17Smallest);
+console.log("p17");
+let p17number1 = 96;
+p17number2 = 100;
+if (p17number1 > p17number2) {
+  console.log("The smallest number:", p17number2);
+} else {
+  console.log("The smallest number:", p17number1);
+}
 // problem 18
-let p18number1 = 1;
+console.log("p18");
+let p18number1 = 29;
 p18number2 = 3;
-p18number3 = 2;
-p18Largest = Math.max(p18number1, p18number2, p18number3);
-console.log("The largest number is: " + p18Largest);
+p18number3 = 10;
+if (p18number1 > p18number2) {
+  if (p18number1 > p18number3) {
+    console.log("The largest number is:", p18number1);
+  } else {
+    console.log("The largest number is:", p18number3);
+  }
+} else {
+  if (p18number2 > p18number3) {
+    console.log("The largest number is:", p18number2);
+  } else {
+    console.log("The largest number is:", p18number3);
+  }
+}
 // problem 19
-let p19number1 = 3;
-p19number2 = 2;
-p19number3 = 1;
+console.log("p19");
+let p19number1 = 10;
+p19number2 = 15;
+p19number3 = 12;
 p19number4 = 4;
-p19Smallest = Math.min(p19number1, p19number2, p19number3, p19number4);
-console.log("The smallest number is: " + p19Smallest);
+smallest = p19number1;
+
+if (p19number2 < smallest) {
+  smallest = p19number2;
+}
+if (p19number3 < smallest) {
+  smallest = p19number3;
+}
+if (p19number4 < smallest) {
+  smallest = p19number4;
+}
+console.log("smallest:", smallest);
+
+// p20
+console.log("p20");
+let p20n1 = 85;
+p20n2 = 75;
+p20n3 = 96;
+p20n4 = 69;
+
+let sum = 0;
+
+if (p20n1 > 80) {
+  sum += p20n1;
+}
+if (p20n2 > 80) {
+  sum += p20n2;
+}
+if (p20n3 > 80) {
+  sum += p20n3;
+}
+if (p20n4 > 80) {
+  sum += p20n4;
+}
+console.log(sum);
+// p21
+console.log("p21");
+let p21n1 = 3;
+p21n2 = 6;
+p21n3 = 2;
+p21n4 = 4;
+
+let mult = 1;
+if (p21n1 < 5) {
+  mult *= p21n1;
+}
+if (p21n2 < 5) {
+  mult *= p21n2;
+}
+if (p21n3 < 5) {
+  mult *= p21n3;
+}
+if (p21n4 < 5) {
+  mult *= p21n4;
+}
+console.log(mult);
+// p22
+console.log("p22");
+let p22n1 = 10;
+p22n2 = 20;
+p22n3 = 5;
+
+let evensum = 0;
+
+if (p22n1 % 2 == 0) {
+  evensum += p22n1;
+}
+if (p22n2 % 2 == 0) {
+  evensum += p22n2;
+}
+if (p22n3 % 2 == 0) {
+  evensum += p22n3;
+}
+console.log(evensum);
+// p23
+console.log("p23");
+let p23n1 = 10;
+p23n2 = 9;
+p23n3 = 5;
+
+let oddsum = 0;
+
+if (p23n1 % 2 !== 0) {
+  oddsum += p23n1;
+}
+if (p23n2 % 2 !== 0) {
+  oddsum += p23n2;
+}
+if (p23n3 % 2 !== 0) {
+  oddsum += p23n3;
+}
+console.log(oddsum);
+// p24
+console.log("p24");
+let p24n1 = 5;
+p24n2 = 2;
+p24n3 = 5;
+
+let count5 = 0;
+
+if (p24n1 == 5) {
+  count5 += 1;
+}
+if (p24n2 == 5) {
+  count5 += 1;
+}
+if (p24n3 == 5) {
+  count5 += 1;
+}
+console.log(count5);
+
+// p25
+console.log("p25");
+let p25n1 = 3;
+p25n2 = 12;
+p25n3 = 8;
+p25n4 = 9;
+
+let dividedby3 = 0;
+
+if (p25n1 % 3 == 0) {
+  dividedby3 += 1;
+}
+if (p25n2 % 3 == 0) {
+  dividedby3 += 1;
+}
+if (p25n3 % 3 == 0) {
+  dividedby3 += 1;
+}
+if (p25n4 % 3 == 0) {
+  dividedby3 += 1;
+}
+console.log(dividedby3);
+
+// p26
+console.log("p26");
+let p26n1 = 7;
+p26n2 = 22;
+p26n3 = 13;
+p26n4 = 30;
+
+let dividedby11 = 0;
+
+if (p26n1 % 11 !== 0) {
+  dividedby11 += p26n1;
+}
+if (p26n2 % 11 !== 0) {
+  dividedby11 += p26n2;
+}
+if (p26n3 % 11 !== 0) {
+  dividedby11 += p26n3;
+}
+if (p26n4 % 11 !== 0) {
+  dividedby11 += p26n4;
+}
+console.log(dividedby11);
+// p27
+console.log("p27");
+let p27n1 = 11;
+if (p27n1 > 10) {
+  console.log("YES");
+}
+// p28
+console.log("p28");
+let p28n1 = 4;
+if (p28n1 < 5) {
+  console.log("YES");
+}
+// p29
+console.log("p29");
+let p29n1 = 3;
+p29n2 = 4;
+p29n3 = 5;
+
+if (p29n1 % 2 == 0) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
+
+if (p29n2 % 2 == 0) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
+
+if (p29n3 % 2 == 0) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
